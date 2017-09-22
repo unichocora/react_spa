f30fe4b0657e897b2240bfa0e7d69004e70ce64b
takedasyohei <uni-chocolate@hotmail.com>
2017-09-22 08:02:47 +0000

    Chapter1 last

---
+++ spa-note/webpack.config.js
@@ -0,0 +1,33 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: [ './src/main.js', './src/main.css' ],
+    output: { path:'/home/ubuntu/workspace/spa-note/public',filename:'bundle.js' },
+    module: {
+        rules:[
+            {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                use:'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    loader:'css-loader!postcss-loader',
+                }),
+            }
+        ],
+    },
+    devtool:'source-map',
+    devServer:{
+        host: '0.0.0.0',
+        contentBase:'./public',
+        port: 8080,
+        // inline:true,
+        // historyApiFallback:true,
+        // disableHostCheck: true,
+    },
+    plugins:[
+        new ExtractTextPlugin({filename: 'bundle.css'}),
+    ],
+};
